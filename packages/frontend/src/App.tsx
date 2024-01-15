e860ba0169e3e399d6bfae8b986b953ee21e3f54
Milossh_ <89982638+hozakowskimilosz@users.noreply.github.com>
2024-01-15 11:54:48 +0100

    Implement responsiveness (#23)

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import {
   Grid,
   Flex,
+  Heading,
   Button,
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
 import columns from "../config/columns";
+import arraySize from "../config/optimalColumnArraySize";
 import colors from "../config/colors";
 import Column from "./components/Column";
 
@@ -16,13 +18,21 @@ function App() {
 
   return (
     <>
-      <Flex>
+      <Flex justify="space-between" align="center">
+        <Heading size={["md", "lg", "xl"]}>Kanban Board</Heading>
+
         <Button onClick={toggleColorMode}>
           {colorMode === "light" ? "🌘" : "🌞"}
         </Button>
       </Flex>
       <Grid
-        templateColumns={`repeat(${columns.length}, 345px)`}
+        templateColumns={[
+          "repeat(1, 1fr)",
+          "repeat(2, 1fr)",
+          "repeat(3, 1fr)",
+          "repeat(4, 1fr)",
+          `repeat(${columns.length >= arraySize ? 5 : 3}, 1fr)`,
+        ]}
         className="grid"
       >
         {columns.map((col) => (
