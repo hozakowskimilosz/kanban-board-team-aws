73eec3a8e0dcedea82e850f1b5acfb76a6b213f0
milosz.hozakowski <mhozakowski@gmail.com>
2024-01-22 12:06:18 +0100

    removed unnecessay lines

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import App from "./App.tsx";
-import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
